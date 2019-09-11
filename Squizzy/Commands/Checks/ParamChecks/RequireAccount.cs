46863d00ca56efe804b980a5c604531f6fb47ceb
Playwo <eliaswolf2001@t-online.de>
2019-09-11 19:37:16 +0200

    Added RequireAccount Check

---
+++ Squizzy/Commands/Checks/ParamChecks/RequireAccount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Qmmands;
+using Squizzy.Entities;
+
+namespace Squizzy.Commands
+{
+    public class RequireAccount : SquizzyParameterCheck
+    {
+        public override ValueTask<CheckResult> CheckAsync(object argument, SquizzyContext context)
+        {
+            if (!(argument is SquizzyPlayer player))
+            {
+                throw new InvalidOperationException("RequireAccount Check can only be used on SquizzyPlayer Parameters");
+            }
+
+            return player.TotalAnsweredQuestions == 0
+                ? CheckResult.Unsuccessful("This player has never used Squizzy before!")
+                : CheckResult.Successful;
+        }
+    }
+}
