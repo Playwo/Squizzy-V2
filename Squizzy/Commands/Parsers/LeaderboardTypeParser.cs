20da5f338e52d920995607bfcf60a955dac83660
Elias Wolf <eliaswolf2001@t-online.de>
2019-09-17 13:55:05 +0200

    Added ability to parse emotes

--- Squizzy/Commands/Parsers/LeaderboardTypeParser.cs
+++ Squizzy/Commands/Parsers/LeaderboardTypeParser.cs
@@ -11,9 +11,9 @@ namespace Squizzy.Commands
     {
         private readonly Dictionary<LeaderboardType, string[]> CategoryShortcuts = new Dictionary<LeaderboardType, string[]>()
         {
-            [new LeaderboardType(Leaderboard.Trophies ,x => x.Trophies, "Trophy", ":trophy:")] = new[] { "Trophies", "Trophie", "Trophy", "Troph" ,"Tro", "Tr", "T" },
+            [new LeaderboardType(Leaderboard.Trophies ,x => x.Trophies, "Trophy", ":trophy:")] = new[] { ":trophy:", "Trophies", "Trophie", "Trophy", "Troph" ,"Tro", "Tr", "T" },
             [new LeaderboardType(Leaderboard.Honor, x => x.Honor, "Honor", ":gem:")] = new[] { "Honor", "Hono", "Hon", "Ho", "H" },
-            [new LeaderboardType(Leaderboard.Magnets, x => x.Magnets, "Magnet", "<:magnet:440898600738750465>")] = new[] { "Magnets", "Magnet", "Magne", "Mags", "Mag", "Ma", "M" },
+            [new LeaderboardType(Leaderboard.Magnets, x => x.Magnets, "Magnet", "<:magnet:440898600738750465>")] = new[] { "<:magnet:440898600738750465>", "Magnets", "Magnet", "Magne", "Mags", "Mag", "Ma", "M" },
             [new LeaderboardType(Leaderboard.SuccessRate, x => x.SuccessRate, "Success Rate", "%")] = new[] { "SuccessRate", "SuccessR", "SRate", "Success", "Rate", "Accuracy" },
             [new LeaderboardType(Leaderboard.AnsweredQuestions, x => x.AnsweredQuestions.Count, "Answered Questions", "x: question:")] = new[] { "AnsweredQuestions", "Answereds", "Answered", "Questions", "Q" , "AQ", "AQS", "QS"}
         };
