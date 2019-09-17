9fcd4a4d32ce0f258a69550be6235b8b861dbf69
Elias Wolf <eliaswolf2001@t-online.de>
2019-09-17 14:08:11 +0200

    Added a simple Command Parser

---
+++ Squizzy/Commands/Parsers/CommandParser.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Qmmands;
+using Squizzy.Extensions;
+
+namespace Squizzy.Commands.Parsers
+{
+    public class CommandParser : SquizzyParser<Command>
+    {
+        public override ValueTask<TypeParserResult<Command>> ParseAsync(Parameter parameter, string value, SquizzyContext context)
+        {
+            var _commands = context.ServiceProvider.GetService<CommandService>();
+
+            var command = _commands.FindCommands(value).FirstOrDefault()?.Command;
+
+            return command == null
+                ? TypeParserResult<Command>.Unsuccessful("Could not find a command matching your input!")
+                : TypeParserResult<Command>.Successful(command);
+        } 
+    }
+}
