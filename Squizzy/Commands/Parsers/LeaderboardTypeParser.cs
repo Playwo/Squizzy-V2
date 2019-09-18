7767968b73912964ec6bc843e67a6622888f7c50
Playwo <eliaswolf2001@t-online.de>
2019-09-18 20:46:36 +0200

    Fixed spacing mistake

--- Squizzy/Commands/Parsers/LeaderboardTypeParser.cs
+++ Squizzy/Commands/Parsers/LeaderboardTypeParser.cs
@@ -15,7 +15,7 @@ namespace Squizzy.Commands
             [new LeaderboardType(Leaderboard.Trophies ,x => x.Trophies, "Trophy", ":trophy:")] = new[] { "Trophies", ":trophy:", "Trophie", "Trophy", "Troph" ,"Tro", "Tr", "T" },
             [new LeaderboardType(Leaderboard.Honor, x => x.Honor, "Honor", ":gem:")] = new[] { "Honor", "Hono", "Hon", "Ho", "H" },
             [new LeaderboardType(Leaderboard.Magnets, x => x.Magnets, "Magnet", "<:magnet:440898600738750465>")] = new[] { "Magnets", "<:magnet:440898600738750465>",  "Magnet", "Magne", "Mags", "Mag", "Ma", "M" },
-            [new LeaderboardType(Leaderboard.AnsweredQuestions, x => x.AnsweredQuestions.Count, "Answered Questions", "x: question:")] = new[] { "AnsweredQuestions", "Answereds", "Answered", "Questions", "Q" , "AQ", "AQS", "QS"}
+            [new LeaderboardType(Leaderboard.AnsweredQuestions, x => x.AnsweredQuestions.Count, "Answered Questions", "x :question:")] = new[] { "AnsweredQuestions", "Answereds", "Answered", "Questions", "Q" , "AQ", "AQS", "QS"}
         };
 
         public override ValueTask<TypeParserResult<LeaderboardType>> ParseAsync(Parameter parameter, string value, SquizzyContext context)
